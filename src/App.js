b402ee1f2e66e53d117a3ac222437a3add1050f3
Adam Tyler <lazy9669@gmail.com>
2018-03-04 23:01:38 -0600

    Remove unreads when opening chat. update last chat time

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import './App.css'
 
 class App extends Component {
 
+  // faking a lot of data and logic without a proper backend to tie this to for demo purposes
   state = {
     chats: [
       {
         id: 1,
         avatar: 'https://pm1.narvii.com/6046/a8c81433f8321b231b72558f13500e0d0d2dd8f4_128.jpg',
         title: 'Arya Stark',
-        subtitle: 'something here', lastUpdate: Date.now(), unread: 3,
+        subtitle: 'something here',
+        lastUpdate: Date.now(),
+        unread: 3,
         messages: [
           { text: 'Winter is coming!', sender: 'Arya Stark', time: '123'},
           { text: 'dont care', sender: 'Cersei', time: '123345'}
@@ -23,7 +26,9 @@ class App extends Component {
         id: 2,
         avatar: 'https://www.e-cigarette-forum.com/data/avatars/m/140/140602.jpg?1429716297',
         title: 'Inigo Montoya',
-        subtitle: 'something here', lastUpdate: Date.now() - 300000, unread: 0,
+        subtitle: 'something here',
+        lastUpdate: Date.now() - 300000,
+        unread: 0,
         messages: [
           { text: 'hi', sender: 'lazy9669', time: '123'},
           { text: 'hello', sender: 'thedude', time: '123345'}
@@ -35,13 +40,18 @@ class App extends Component {
 
   onChatListClick = (chat, e) => {
     console.log('chatListClick', chat, e)
-    // this.setState({ chatsOpen: [...new Set([chat, ...this.state.chatsOpen])] })
-    this.setState({ chatIdsOpen: [...new Set([chat.id, ...this.state.chatIdsOpen])] })
+    // set unread to zero on an opened chat
+    let newChats = this.state.chats.map(item => {
+      if (item.id === chat.id) {
+        item.unread = 0
+      }
+      return item
+    })
+    this.setState({ chats: newChats, chatIdsOpen: [...new Set([chat.id, ...this.state.chatIdsOpen])] })
   }
 
   onClose = (chat, e) => {
     console.log('close', chat, e)
-    // this.setState({ chatsOpen: this.state.chatsOpen.filter(item => item.id !== chat.id) })
     this.setState({ chatIdsOpen: this.state.chatIdsOpen.filter(id => id !== chat.id) })
   }
 
@@ -75,6 +85,7 @@ class App extends Component {
     let newChats = this.state.chats.map(item => {
       if (item.id === chat.id) {
         item.messages = [...item.messages, newMsg]
+        item.lastUpdate = Date.now()
       }
       return item
     })
